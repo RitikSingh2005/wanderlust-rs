235b649c77299e56c4980b399af418d8475545aa
RitikSingh2005 <hritiksingh5002@gmail.com>
2026-02-06 22:55:20 +0530

    add project files

---
+++ public/js/map.js
@@ -0,0 +1,21 @@
+mapboxgl.accessToken =mapToken ;
+ 
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style:"mapbox://styles/mapbox/streets-v12",
+  center: listing.geometry.coordinates, // starting position [longtitude, latitude]
+  zoom: 9 // starting zoom
+});
+
+
+const marker = new mapboxgl.Marker({color:'red'})
+ .setLngLat(listing.geometry.coordinates)//Listing.geometry.coordinates
+ .setPopup(new mapboxgl.Popup({offset: 25})
+ .setHTML(`<h4> ${listing.title} </h4><p>Exact location will be provided after booking</p>`))
+ .addTo(map);
+
+
+map.addControl(new mapboxgl.ScaleControl());
+map.addControl(new mapboxgl.NavigationControl());
+
+
